66339ab3d2f0242af4e58ec80ce65eebf4b8b927
Thien Le <thien.le@isobar.com>
2020-04-26 17:03:03 +0700

    [04182026] Ecommerce

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -251,6 +251,9 @@ class ProductController extends Controller
             'formRating'        => $formRating->createView(),
             'formCheckout'      => $formCheckout->createView(),
             'rating'            => !empty($rating['ratingValue']) ? str_replace('.0', '', number_format($rating['ratingValue'], 1)) : 0,
+            'ratingPercent'     => str_replace('.00', '', number_format(($rating['ratingValue'] * 100) / 5, 2)),
+            'ratingValue'       => round($rating['ratingValue']),
+            'ratingCount'       => round($rating['ratingCount']),
             'listOfRating'      => $listOfRating
         ]);
     }
