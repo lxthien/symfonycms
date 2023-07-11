764b35239ca8f834a6a60f1d4e20d80b01989a61
Thien Le <lxthien@gmail.com>
2023-07-11 16:18:22 +0700

    [11072023] Convert webp images

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -319,8 +319,8 @@ class NewsController extends Controller
             $img->setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
             $img->setAttribute('data-src', $src);
             $img->setAttribute('alt', $alt);
-            $img->setAttribute('width', !empty($width) ? $width : 500);
-            $img->setAttribute('height', !empty($height) ? $height : 500);
+            $img->setAttribute('width', !empty($width) ? $width > 900 ? 900 : $width : 500);
+            $img->setAttribute('height', !empty($height) ? $width > 900 ? round(($height*900)/$width) : $height : 500);
             $img->setAttribute('class', 'lazyload');
         }
         
@@ -547,7 +547,7 @@ class NewsController extends Controller
             ->findBy(
                 array('postType' => 'post', 'enable' => 1),
                 array('viewCounts' => 'DESC'),
-                15
+                10
             );
 
         return $this->render('news/hot.html.twig', [
