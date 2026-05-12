cf66ed5a69d186a1e699cb10bf43773adc769fff
Thien Le <lxthien@gmail.com>
2026-05-12 08:02:55 +0700

    [07052026] Update UI

--- web/assets/ivoryckeditor/config.js
+++ web/assets/ivoryckeditor/config.js
@@ -14,8 +14,13 @@ CKEDITOR.editorConfig = function( config ) {
     config.htmlEncodeOutput = false;
     config.entities = false;
     config.basicEntities = false;
-    config.extraPlugins = "toc";
-    config.extraAllowedContent = 'blockquote(expert-quote); div(stat-box); span(stat-number,stat-label,stat-source); p(author-byline,author-review); div(key-takeaways,definition,table-of-contents,note,table(summary-table))';
+    config.extraPlugins = 'toc,image2,widget,lineutils,widgetselection';
+    config.removePlugins = 'image';
+    config.extraAllowedContent = 'img[data-fancybox,data-caption]; a[data-fancybox,data-caption]; blockquote(expert-quote); div(stat-box); span(stat-number,stat-label,stat-source); p(author-byline,author-review); div(key-takeaways,definition,table-of-contents,note,table(summary-table)); figure(*)[*]; figcaption(*)[*]; img[*](*); script[*]; style[*]';
+    config.allowedContent = true;
+    // Protect script tags from being stripped in Source mode
+    config.protectedSource.push(/<script[\s\S]*?<\/script>/gi);
+    config.protectedSource.push(/<style[\s\S]*?<\/style>/gi);
     config.toolbar_MyEditor =
     [
         { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
