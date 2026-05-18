cdb9860be4d14c6b5f9e8ac2699f8ee31981597b
Thien Le <lxthien@gmail.com>
2026-05-18 18:16:38 +0700

    Thêm test tối thiểu cho SEO analyzer, slug/redirect, comment, publish workflow.
    .\vendor\bin\phpunit.bat --testsuite "Project Test Suite"

---
+++ tests/AppBundle/Entity/CommentTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\News;
+use PHPUnit\Framework\TestCase;
+
+class CommentTest extends TestCase
+{
+    public function testCommentKeepsDoctrineRelations()
+    {
+        $news = new News();
+        $parent = new Comment();
+
+        $comment = new Comment();
+        $comment
+            ->setNews($news)
+            ->setParent($parent);
+
+        $this->assertSame($news, $comment->getNews());
+        $this->assertSame($parent, $comment->getParent());
+        $this->assertNull($comment->getNewsId());
+        $this->assertNull($comment->getCommentId());
+    }
+
+    public function testAddCommentAssignsNewsRelation()
+    {
+        $news = new News();
+        $comment = new Comment();
+
+        $news->addComment($comment);
+
+        $this->assertSame($news, $comment->getNews());
+        $this->assertTrue($news->getComments()->contains($comment));
+    }
+
+    public function testSpamGuardRejectsAtSign()
+    {
+        $comment = new Comment();
+        $comment->setContent('spam@example.com');
+
+        $this->assertFalse($comment->isLegitComment());
+
+        $comment->setContent('Nội dung bình luận hợp lệ');
+
+        $this->assertTrue($comment->isLegitComment());
+    }
+}
